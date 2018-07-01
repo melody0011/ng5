1bd1f12b814f55cac0765780355a45720eb93328
mike <melody_relax@hotmail.com>
2018-07-01 10:25:49 +0700

    บันถึกครั้งแรก

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  itemCount: number = 4;
+  btnText: string = 'Add an Item';
+goalText: string = 'My first life goal'; // Add this
+goals = [];
+  constructor() {}
+
+  ngOnInit() {
+    this.itemCount = this.goals.length;
+  }
+  addItem() {
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
+    }
